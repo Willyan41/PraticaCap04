99e71afd24219fe54a7e2d3f2a1030fdf258a680
Willyan Santos <willyan41@hotmail.com>
2020-03-27 20:22:55 -0300

    Adicionar arquivos de projeto.

---
+++ PraticaCap04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace PraticaCap04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("O Valor da Circunferencia {0}", circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O Valor do Volume {0}", volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: {0}", Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+}
